338ae20ad6b5eda26b8df899d4a822a29a746554
isaasoemo <731217@student.dusd.net>
2026-03-17 11:14:53 -0700

    finidshed project
    do challange  at home bro

--- P4BasicGameplayGomez/Assets/Course Library/Scripts/PlayerController.cs
+++ P4BasicGameplayGomez/Assets/Course Library/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
 
     public GameObject projectilePrefab;
 
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,8 +33,20 @@ public class PlayerController : MonoBehaviour
                 transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
             }
         }
+        if(transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+
+        if(transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
